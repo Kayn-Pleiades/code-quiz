4ad109977b6b6cab056fb0a9df4f66b72518eef6
unknown <kayn.pleiades@gmail.com>
2021-05-13 20:51:57 -0400

    initial question

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Questions
-const CodeQuestions = [
+const codeQuestions = [
     {
         question: "What does HTML stand for?",
         choices: ["Hyper Text Markup Language", "Hardware Text Maker Language", "Hypertext Trait Making Language", "Happy Toads Marry Lovers"],
         answer: "Hyper Text Markup Language"
     }
 ];
+
+// Load a question when the page loads
+function init() {
+    $("#question").text(codeQuestions[0].question);
+    for (i = 0; i < codeQuestions[0].choices.length; i++) {
+        var choice = codeQuestions[0].choices[i];
+        var code = `
+            <br>    
+            <button type="submit" class="btn btn-primary"> ${choice} </button>
+            <br>
+    `;
+        $("#choices").append(code);
+    }
+};
+
+init();
