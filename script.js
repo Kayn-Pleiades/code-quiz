fa30c093796d39c6b8991452e736a07636998b9e
unknown <kayn.pleiades@gmail.com>
2021-05-13 20:18:30 -0400

    sample question

---
+++ script.js
@@ -0,0 +1,8 @@
+// Questions
+const CodeQuestions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hardware Text Maker Language", "Hypertext Trait Making Language", "Happy Toads Marry Lovers"],
+        answer: "Hyper Text Markup Language"
+    }
+];
