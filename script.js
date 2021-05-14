f6f88c7a27acbb84d4898c4bbff4e847afea820b
unknown <kayn.pleiades@gmail.com>
2021-05-14 19:18:32 -0400

    now second question buttons respond

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const codeQuestions = [
         question: "What does HTML stand for?",
         choices: ["Hyper Text Markup Language", "Hardware Text Maker Language", "Hypertext Trait Making Language", "Happy Toads Marry Lovers"],
         answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "How do you add a comment to a JavaScript file?",
+        choices: ["!!", "!--", "//", "* *"],
+        answer: "//",
     }
 ];
 
@@ -22,31 +27,48 @@ function init() {
     }
 };
 
+// Runs when page loads
+init();
+
 // Load the next question
-function nextQuestion() {
-    console.log("ready");
+function nextQuestion(lastQuestion) {
+    if (lastQuestion !== 10) {
+        var currentQuestion = lastQuestion + 1;
+        $("#questionNumber").text(currentQuestion);
+        $("#question").text(codeQuestions[lastQuestion].question);
+        $("#choices").html("");
+        for (i = 0; i < codeQuestions[lastQuestion].choices.length; i++) {
+            var choice = codeQuestions[lastQuestion].choices[i];
+            var code = `
+                <br>    
+                <button type="button" class="btn btn-primary"> ${choice} </button>
+                <br>
+        `;
+            $("#choices").append(code);
+        }
+    }
+    else {
+        console.log("end");
+    }
 }
 
 // When a choice is clicked on
-$(document).ready(function () {
-    $("button").on("click", function () {
-        var choice = $(this).text().trim();
-        var whichQuestion = $("#questionNumber").text().trim();
-        var questionNumber = parseInt(whichQuestion);
-        var arrayNumber = questionNumber - 1;
-        if (choice === codeQuestions[arrayNumber].answer) {
-            $(this).removeClass("btn-primary").addClass("btn-success");
-            var score = $("#score").text().trim();
-            var toNumber = parseInt(score);
-            var newScore = toNumber + 1;
-            $("#score").text(newScore);
-            setTimeout(function() {nextQuestion()}, 500);
-        }
-        else {
-            $(this).removeClass("btn-primary").addClass("btn-danger");
-        }
-    });
+$("#choices").on('click', 'button', function (event) {
+    event.preventDefault();
+    var choice = $(this).text().trim();
+    console.log(choice);
+    var whichQuestion = $("#questionNumber").text().trim();
+    var questionNumber = parseInt(whichQuestion);
+    var arrayNumber = questionNumber - 1;
+    if (choice === codeQuestions[arrayNumber].answer) {
+        $(this).removeClass("btn-primary").addClass("btn-success");
+        var score = $("#score").text().trim();
+        var toNumber = parseInt(score);
+        var newScore = toNumber + 1;
+        $("#score").text(newScore);
+        setTimeout(function () { nextQuestion(questionNumber) }, 500);
+    }
+    else {
+        $(this).removeClass("btn-primary").addClass("btn-danger");
+    }
 });
-
-// Runs when page loads
-init();
